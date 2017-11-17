0b378f77e493ad29e560b04bf149999f898548e3
OleksandrMakeiev <bonusshop2010@gmail.com>
2017-11-17 11:46:48 +0200

    так как вы указали в коментарии, не работает....

---
+++ tests.py
@@ -0,0 +1,14 @@
+import math
+
+numeric = int(input("Введите целое число: "))
+x = numeric % 2
+
+def is_even (x):
+    if is_even(x):
+        print("%d - четное число" % x)
+        return True
+    else:
+        print("%d - нечетное число" % x)
+        return False
+
+is_even(x)
