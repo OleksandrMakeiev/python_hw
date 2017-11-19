8859e0415e0538bfead6940f6dfcd442cf3539d7
OleksandrMakeiev <bonusshop2010@gmail.com>
2017-11-19 20:34:28 +0200

    Вроде все ок....

--- HW 13_16.py
+++ HW 13_16.py
@@ -21,16 +21,15 @@ else:
 print()
 print('--------------------------------------------------------------------------------------------------------')
 print()
-print(
-    "15 Написать функцию, которая отвечает на вопрос, пересекаются ли две заданные окружности на плоскости.\nКаждая окружность задается координатами центра и радиусом.")
+print("15 Написать функцию, которая отвечает на вопрос, пересекаются ли две заданные окружности на плоскости.\nКаждая окружность задается координатами центра и радиусом.")
 
 def is_intersected(x1, y1, x2, y2, radius1, radius2):
-    if center_to_center > (radius1 + radius2):
-        return True
-    elif center_to_center < abs(radius1 - radius2):
-        return True
-    elif abs(center_to_center - (radius1 - radius2)) < 0.1:
+    if center_to_center <= (radius1 + radius2):
         return False
+        if center_to_center < abs(radius2 - radius1):
+            return True
+        else:
+            return True
     else:
         return False
 
@@ -40,32 +39,29 @@ y1 = int(input("Введите значение координат по оси Y
 y2 = int(input("Введите значение координат по оси Y второй окружности: "))
 radius1 = int(input("Введите радиус первой окружности: "))
 radius2 = int(input("Введите радиус второй окружности: "))
-center_to_center = math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+center_to_center = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
 is_intersected(x1, y1, x2, y2, radius1, radius2)
 
 if is_intersected(x1, y1, x2, y2, radius1, radius2):
     print("Точек пересечения нет")
-elif center_to_center < abs(radius1 - radius2):
+if center_to_center < abs(radius1 - radius2):
     print("Точек пересечения нет")
-elif abs(center_to_center - (radius1 - radius2)) < 0.1:
-    print("2 точки пересечения")
 else:
     print("Окружности касаются")
 
 print()
 print('--------------------------------------------------------------------------------------------------------')
 print()
-print(
-    "16 Два поезда движутся на скорости V1 и V2 навстречу друг другу. Между ними 10 км. пути.\nЧерез 4 км пути первый поезд может свернуть на запасной путь. При заданных скоростях узнать столкнутся ли поезда.")
+print("16 Два поезда движутся на скорости V1 и V2 навстречу друг другу. Между ними 10 км. пути.\nЧерез 4 км пути первый поезд может свернуть на запасной путь. При заданных скоростях узнать столкнутся ли поезда.")
 
 def is_train_crash(speed1, distance, speed2, distance_train1):
     distance_train2 = distance - distance_train1
     time1 = distance_train1 / speed1
     time2 = distance_train2 / speed2
     if time1 > time2:
-        return False
-    else:
         return True
+    else:
+        return False
 
 speed1 = int(input("Введите скорость первого поезда: "))
 speed2 = int(input("Введите скорость второго поезда: "))
@@ -74,6 +70,6 @@ distance_train1 = 4
 is_train_crash(speed1, distance, speed2, distance_train1)
 
 if is_train_crash(speed1, distance, speed2, distance_train1):
-    print("Столкновения избежали.")
+    print("Поезда столкнулись")
 else:
-    print("Поезда столкнулись!")
+    print("Столкновения избежали.")
