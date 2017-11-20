f86336d7dfacc58bc13c83dec764d4990acf8b55
OleksandrMakeiev <bonusshop2010@gmail.com>
2017-11-20 09:30:03 +0200

    Task #17

---
+++ solve_quadratic_equation.py
@@ -0,0 +1,25 @@
+import math
+print('Home Work 19.11.17')
+print('----------------------------------------------------------------------------------------------')
+print()
+print('17 Написать функцию решения квадратного уравнения.\nОпределение функции:def solve_quadratic_equation(a, b, c): # return 2 valuespass')
+print()
+
+print('Введите значения a, b, c для квадратного уравнения такого типа: ax^2 + bx + c = 0')
+
+def solve_quadratic_equation(a, b, c, none=None):
+    discriminant = b ** 2 - 4 * a * c
+    if discriminant > 0:
+        x1 = (-b + math.sqrt(discriminant)) / (2 * a)
+        x2 = (-b - math.sqrt(discriminant)) / (2 * a)
+        return (x1, x2)
+    elif discriminant == 0:
+        x = -b / (2 * a)
+        return (x, none)
+    else:
+        return (none, none)
+
+a = (float(input("a = ")))
+b = (float(input("b = ")))
+c = (float(input("c = ")))
+solve_quadratic_equation(a, b, c)
