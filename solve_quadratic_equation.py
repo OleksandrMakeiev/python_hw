7172e1ac357b9dd58b0bf020bd524765e7f7e197
OleksandrMakeiev <bonusshop2010@gmail.com>
2017-11-20 09:39:06 +0200

    Fix result

--- solve_quadratic_equation.py
+++ solve_quadratic_equation.py
@@ -8,10 +8,7 @@ print()
 print('Введите значения a, b, c для квадратного уравнения такого типа: ax^2 + bx + c = 0')
 
 def solve_quadratic_equation(a, b, c, none=None):
-    discriminant = b ** 2 - 4 * a * c
     if discriminant > 0:
-        x1 = (-b + math.sqrt(discriminant)) / (2 * a)
-        x2 = (-b - math.sqrt(discriminant)) / (2 * a)
         return (x1, x2)
     elif discriminant == 0:
         x = -b / (2 * a)
@@ -22,4 +19,14 @@ def solve_quadratic_equation(a, b, c, none=None):
 a = (float(input("a = ")))
 b = (float(input("b = ")))
 c = (float(input("c = ")))
+discriminant = b ** 2 - 4 * a * c
+x1 = (-b + math.sqrt(discriminant)) / (2 * a)
+x2 = (-b - math.sqrt(discriminant)) / (2 * a)
 solve_quadratic_equation(a, b, c)
+
+if discriminant > 0:
+    print("x1 = %.2f \nx2 = %.2f" % (x1, x2))
+elif discriminant == 0:
+    print("x = %.2f" % x)
+else:
+    print("Корней нет")
