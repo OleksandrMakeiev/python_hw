b2de0f23f76794db99d8dd6900b299a412f9a2d3
OleksandrMakeiev <bonusshop2010@gmail.com>
2017-11-23 09:46:19 +0200

    Исправил баги... Версия 1 с прошлого занятия

--- def find_max.py
+++ def find_max.py
@@ -1,16 +1,19 @@
 # 19
 import random
 
-num_limit = 100
-lower_bound = (int(input("Введите нижнюю границу поиска ")))
-upper_bound = (int(input("Введите верхнюю границу поиска ")))
-
+num_limit = (int(input("Введите количество случайных чисел, которые выберет программа:  ")))
+lower_bound = (int(input("Введите нижнюю границу поиска: ")))
+upper_bound = (int(input("Введите верхнюю границу поиска: ")))
 
 def find_max(num_limit, lower_bound, upper_bound):
-    max_number = upper_bound
-    for i in range (num_limit):
+    max_number = lower_bound
+
+    for i in range(num_limit):
         number = random.randint(lower_bound, upper_bound)
-        if number <= max_number:
+        #print('Случайное число: ',number)
+        if number > max_number:
             max_number = number
-            return max_number
-print("Мвксимальное случайное число из поиска равно %d" % find_max(num_limit, lower_bound, upper_bound))
+    return max_number
+    print(max_number)
+
+print("Максимальное случайное число из поиска равно %d" % find_max(num_limit, lower_bound, upper_bound))
