35c5a1dfdad63d91f3ca676a2b110d397f5ec679
OleksandrMakeiev <bonusshop2010@gmail.com>
2017-11-23 09:21:20 +0200

    оптимизировал код, старый кусок закаментировал для своего понимания

--- def sum_symbol_codes.py
+++ def sum_symbol_codes.py
@@ -6,11 +6,13 @@ def sum_symbol_codes(first, last):
     last_symbol = ord(last)
     sum_symbol = 0
     if first_symbol <= last_symbol:
-        min_value = first_symbol
-        max_value = last_symbol
-    else:
-        min_value = last_symbol
-        max_value = first_symbol
+        min_value = min(first_symbol, last_symbol)
+        max_value = max(first_symbol, last_symbol)
+        # min_value = first_symbol
+        # max_value = last_symbol
+    # else:
+    #     min_value = last_symbol
+    #     max_value = first_symbol
     for i in range (min_value, max_value +1):
             sum_symbol += i
     return sum_symbol
