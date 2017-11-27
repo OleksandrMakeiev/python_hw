0836fef7c8835cb6ffa34a7558622346855ae6b1
OleksandrMakeiev <bonusshop2010@gmail.com>
2017-11-27 11:35:11 +0200

    поубирал внешние скобки

--- def find_max.py
+++ def find_max.py
@@ -1,16 +1,16 @@
 # 19
 import random
 
-num_limit = (int(input("Введите количество случайных чисел, которые выберет программа:  ")))
-lower_bound = (int(input("Введите нижнюю границу поиска: ")))
-upper_bound = (int(input("Введите верхнюю границу поиска: ")))
+num_limit = int(input("Введите количество случайных чисел, которые выберет программа:  "))
+lower_bound = int(input("Введите нижнюю границу поиска: "))
+upper_bound = int(input("Введите верхнюю границу поиска: "))
 
 def find_max(num_limit, lower_bound, upper_bound):
     max_number = lower_bound
 
     for i in range(num_limit):
         number = random.randint(lower_bound, upper_bound)
-        #print('Случайное число: ',number)
+        print('Случайное число: ',number)
         if number > max_number:
             max_number = number
     return max_number
