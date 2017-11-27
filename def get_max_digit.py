e2b2def7a20501034178ba749e0267f2089fab30
OleksandrMakeiev <bonusshop2010@gmail.com>
2017-11-27 11:47:30 +0200

    21

---
+++ def get_max_digit.py
@@ -0,0 +1,15 @@
+# 21
+import random
+
+number = random.randint(100000000000, 999999999999)
+print("Случайное 12-ти значное число:", number)
+
+
+def get_max_digit(number):
+    list = [int(dig) for dig in (str(number))]
+    max_digit = max(list)
+
+    return max_digit
+
+
+print("Самая большая цифра в числе: ", get_max_digit(number))
