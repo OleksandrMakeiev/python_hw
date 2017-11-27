6e654d15015dd779c84688d1fbad471938c9039e
OleksandrMakeiev <bonusshop2010@gmail.com>
2017-11-27 14:31:25 +0200

    fixed

--- random digit.py
+++ random digit.py
@@ -2,7 +2,7 @@
 import random
 
 print("Программа загадывает целое число от 1 до 10, Ваша задача отдгадать его, следуя подсказкам программы. Для выхода нажми q ")
-number = random.randint(1, 10)
+number = random.randint(1, 11)
 while True:
 
     choice = int(input("Сделай свой выбор [1-10]: "))
