82a2ce052cec2977745d7ecdf990247b97fa859d
OleksandrMakeiev <bonusshop2010@gmail.com>
2017-12-05 12:21:17 +0200

    29 fix

--- def gen_password.py
+++ def gen_password.py
@@ -1,9 +1,11 @@
+#29
 import random
 import string
 
 def gen_password():
     symbol = "_"
     chars = string.ascii_uppercase + string.ascii_lowercase + string.digits + symbol
-    return ''.join(random.choice(chars) for x in range(8))
+    while True:
+        return ''.join(random.choice(chars) for x in range(8))
 
 print(gen_password())
