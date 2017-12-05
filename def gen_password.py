c0664ab3db13f244a5ddc39abf7e1eb5e27bdf7f
OleksandrMakeiev <bonusshop2010@gmail.com>
2017-12-05 14:43:56 +0200

    29

--- def gen_password.py
+++ def gen_password.py
@@ -5,7 +5,7 @@ import re
 
 def gen_password():
     symbol = "_"
-    chars = string.ascii_uppercase + string.ascii_lowercase + string.digits  + symbol*5
+    chars = string.ascii_uppercase + string.ascii_lowercase + string.digits  + symbol
     while True:
         pswd = ''.join(random.choice(chars) for x in range(8))
         if (re.findall('[A-Z]', pswd)) and (re.findall('[a-z]', pswd)) and (re.findall('[0-9]', pswd)):
