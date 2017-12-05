b526ffd3a77294ba20052a1e6ab0dc3aed9d68bb
OleksandrMakeiev <bonusshop2010@gmail.com>
2017-12-05 12:09:38 +0200

    # 29 optimization code

--- def gen_password.py
+++ def gen_password.py
@@ -1,13 +1,9 @@
 import random
+import string
 
 def gen_password():
-    upper = "QWERTYUIOPASDFGHJKLZXCVBNM"
-    lower = "qwertyuiopasdfghjklzxcvbnm"
-    digits = "0123456789"
     symbol = "_"
-
-    chars = upper + lower + digits + symbol
-    size = random.randint(8, 8)
-    return ''.join(random.choice(chars) for x in range(size))
+    chars = string.ascii_uppercase + string.ascii_lowercase + string.digits + symbol
+    return ''.join(random.choice(chars) for x in range(8))
 
 print(gen_password())
