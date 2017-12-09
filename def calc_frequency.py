c6fcd4f5a9f9301d73b98c1a1b6bb5ea97b48fea
OleksandrMakeiev <bonusshop2010@gmail.com>
2017-12-10 01:46:19 +0200

    26

---
+++ def calc_frequency.py
@@ -0,0 +1,28 @@
+#26
+import random
+lst = [random.randint(-1, 1) for i in range(11)]
+print(lst)
+
+
+# print(first, second, third)
+
+def calc_frequency(lst):
+    first = lst.count(-1)
+    second = lst.count(1)
+    third = lst.count(0)
+    results = []
+    for c in lst:
+        if first > second  and first > third:
+            results = first
+            print("-1 cамое частое число и оно встречается ", results)
+        elif second > first and second > third:
+            results = second
+            print("1 cамое частое число и оно встречается ", results)
+        elif third > first and third > second:
+            results = third
+            print("0 cамое частое число и оно встречается ", results)
+        else:
+            return None
+calc_frequency(lst)
+
+
